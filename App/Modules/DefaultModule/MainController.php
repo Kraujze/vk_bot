6bb067614186d6c00e2233fc12a6578377514a81
Kraujze <kraujze@inbox.ru>
2021-03-10 22:24:17 +0300

    Added loading of module triggers and method call by trigger

--- App/Modules/DefaultModule/MainController.php
+++ App/Modules/DefaultModule/MainController.php
@@ -6,16 +6,35 @@ class MainController extends Controller {
 
     private $data;
     private $command;
+    private static $commands = [];
     private $params = [];
 
     public function __construct(object $data, string $command, array $params) {
         $this->data = $data;
         $this->command = $command;
         $this->params = $params;
-        if ($command = 'лёша') { parent::sendMessage('Лёша не лох, он красава!', $data->peer_id); } else { parent::sendMessage($command, $data->peer_id); }
+        self::$commands = require(__DIR__.'Commands.php');
+        //if ($command = 'лёша') { parent::sendMessage('Лёша не лох, он красава!', $data->peer_id); } else { parent::sendMessage($command, $data->peer_id); }
+
+        foreach (self::$commands as $key => $cmd) {
+            if (in_array($command, $cmd['triggers'])) {
+                $execute = $cmd;
+            } else {
+                $execute = 'default';
+            }
+        }
+        $this->$execute();
+    }
+
+    public function help() {
+        echo 'help';
+        parent::sendMessage('Хелп', $this->data->peer_id);
+    }
+    public function test() {
+        parent::sendMessage('Тест', $this->data->peer_id);
+    }
+    public function default() {
+        parent::sendMessage('Дефолт', $this->data->peer_id);
     }
 //
-    public function execute() {
-
-    }
 }
